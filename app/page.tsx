118c1193b5972384e535060dcc25c44766e312c6
Aditya Mehta <ronniemehta9711@gmail.com>
2025-07-22 01:32:14 +0530

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
-        Welcome to Next.js Ronnie👋
+      <h1 className="text-3xl text-light-500 font-black">
+        Welcome to Next.js👋
       </h1>
+      <h1 className="h1-bold">Welcome to Next.js👋</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js👋</h1>
     </div>
   );
 };
